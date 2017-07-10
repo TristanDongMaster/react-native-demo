536b29136f1df6832454303b3d7cb0fd57760739
tristan.dong <tristan.dong@vipshop.com>
2017-07-10 14:05:02 +0800

    -mupdate

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ import ChatScreen from './pages/ChatScreen.js'
 import TestScreen from './pages/TestScreen.js'
 import WebViewExample from './pages/WebView.js'
 
+import { Provider,connect } from 'react-redux'
+import configStore from './redux/store'
+// import actions
+import {moveBrick, robBank, goBroke} from './redux/actions'
+// initialState
+import initialState from './redux/store'
+
 
 const AwesomeProject = StackNavigator({
   Home: { screen: HomeScreen },
